b4df177de3f6db763cc1f3c3e381817437b3c889
gfpk <nci@nci-VirtualBox.(none)>
2014-04-08 14:52:17 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aria::Application.config.secret_token = '02a690963bdfa7030acfedf22888441fbea93289498c690b68c7185df5e6d4e4439e99eadf2107fcd933057ea4dc550e25912c15a2c377fdb834343dc0dd4752'
