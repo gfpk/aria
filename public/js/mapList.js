648c3e96f7fdd0e10390a6f70792cf806d81aae1
Greg <kongu3333@gmail.com>
2014-04-16 16:46:36 +0100

    fast one

--- public/js/mapList.js
+++ public/js/mapList.js
@@ -1,7 +1,19 @@
 var mapList = angular.module('mapList', []);
  
-	mapList.controller('MapListCtrl', function ($scope, $http) {
-		$http.get('http://127.0.0.1:3000/json/maps.json').success(function(data) {
-		$scope.maps = data;
+	mapList.controller('mapListCtrl', ['$scope', '$http', 
+		function ($scope, $http) {
+			$http.get('http://127.0.0.1:3000/json/maps.json').success(function(data) {
+				$scope.maps = data;
+				$scope.getEntities = (function(){
+					return(alert('boo'))});
+
+
+				
 	});
+}]);
+
+mapList.directive('makeMap', function() {
+	return {
+		template:"{{map}}"
+	};
 });
