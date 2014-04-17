3cc090a559480fa46c033d96790d6a871012d109
Greg <kongu3333@gmail.com>
2014-04-17 10:10:00 +0100

    testing timeout in angular to get maplist working

--- public/js/mapList.js
+++ public/js/mapList.js
@@ -1,19 +1,21 @@
 var mapList = angular.module('mapList', []);
  
-	mapList.controller('mapListCtrl', ['$scope', '$http', 
-		function ($scope, $http) {
+	mapList.controller('mapListCtrl', ['$scope', '$http', '$timeout',
+		function ($scope, $http, $timeout) {
 			$http.get('http://127.0.0.1:3000/json/maps.json').success(function(data) {
 				$scope.maps = data;
-				$scope.getEntities = (function(){
-					return(alert('boo'))});
+				$scope.rendermap = function(e){console.log(e)};
+				$scope.testTimeOutfunction = function(){
+			        var mapboxId = "gfpk.map-z6zqpvk6";
+			        var arr = [1,2,3,3,4,4,4,5];
+			        for(var i=0;i<arr;i++){
+			        	console.log(arr);
+			        }
 
+			    };
+				$timeout($scope.testTimeOutfunction(),100);
 
 				
 	});
 }]);
 
-mapList.directive('makeMap', function() {
-	return {
-		template:"{{map}}"
-	};
-});
