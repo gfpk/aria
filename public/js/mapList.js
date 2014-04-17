0b9cb88b9d0c0db932b8ad8f368f1819aa108014
Greg <kongu3333@gmail.com>
2014-04-17 11:33:27 +0100

    maplist working using g'al plain js and timeout

--- public/js/mapList.js
+++ public/js/mapList.js
@@ -1,21 +1,26 @@
 var mapList = angular.module('mapList', []);
  
-
-	mapList.controller('mapListCtrl', ['$scope', '$http', '$timeout',
+	mapList.controller('mapListCtrl', ['$scope', '$http','$timeout',
 		function ($scope, $http, $timeout) {
 			$http.get('http://127.0.0.1:3000/json/maps.json').success(function(data) {
 				$scope.maps = data;
-				$scope.rendermap = function(e){console.log(e)};
-				$scope.testTimeOutfunction = function(){
+				$scope.rendermaps = (function(){
 			        var mapboxId = "gfpk.map-z6zqpvk6";
-			        var arr = [1,2,3,3,4,4,4,5];
-			        for(var i=0;i<arr;i++){
-			        	console.log(arr);
+			        var arr = ($scope.maps);
+			        for(var i =0;i<arr.length;i++){
+			        	var elId ="map-" + String(i); 	
+			        	var elem = document.getElementById(elId);
+			        	var mapData = arr[i];
+			        	console.log(mapData);
+			        	L.mapbox.map(elId, mapboxId).setView(mapData.geo.center, mapData.geo.zoom);;
+
+
+
 			        }
+			       
+			    });
 
-
-			    };
-				$timeout($scope.testTimeOutfunction(),100);
+				$timeout($scope.rendermaps,1000);
 
 				
 	});
--- public/js/mapbox.js
+++
@@ -1,8 +0,0 @@
-document.ready(function(function(){
-	var mapCanvases = document.getElementsByClassName('map'); 
-
-	for(var i = 0; i<mapCanvases;i++){
-		var mapdata = mapCanvases[i].name
-		console.log(mapdata)
-
-}})();
