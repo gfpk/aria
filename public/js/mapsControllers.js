f0c6fab2bf60a1a3b601876d9c8601e04c20e2a8
Grzegorz Filipek <kongu3333@gmail.com>
2014-04-22 23:47:18 +0100

    writing to local storage

--- public/js/mapsControllers.js
+++ public/js/mapsControllers.js
@@ -42,36 +42,58 @@ mapsControllers.controller('SinglePhoneCtrl', ['$scope', '$routeParams', '$http'
 	function($scope, $routeParams, $http, $timeout) {
 		$http.get('http://127.0.0.1:3000/json/maps.json').success(function(data) {
 
-			//$scope.mapNo = $routeParams.mapNo;
-			$scope.singlemapdata = data[$routeParams.mapNo];
 			
+			if(localStorage.getItem('singlemapdata'+ $routeParams.mapNo)){
+				
+				$scope.singlemapdata = JSON.parse(localStorage.getItem('singlemapdata'+ $routeParams.mapNo));
+				console.log(typeof($scope.singlemapdata.geoJSON))
+			}else{
+				$scope.singlemapdata = data[$routeParams.mapNo];
+				console.log(typeof($scope.singlemapdata.geoJSON))
+				
+			};
 			$scope.rendermap = function(){
 				var mapboxId = "gfpk.map-z6zqpvk6";
 				zeMap = L.mapbox.map ('map', mapboxId).setView($scope.singlemapdata.geo.center, ($scope.singlemapdata.geo.zoom + 1));
 				if($scope.singlemapdata.geoJSON){
-
+							console.log($scope.singlemapdata.geoJSON.length);
 			        		zeLayer = L.mapbox.featureLayer().addTo(zeMap);
+			        		
 
-							var feature = $scope.singlemapdata.geoJSON
-
-							zeLayer.setGeoJSON(feature);
-							zeLayer.on('ready', function() {
-							    zeMap.fitBounds(featureLayer.getBounds());
-							});
+							zeLayer.setGeoJSON($scope.singlemapdata.geoJSON);
+							
 			       			
 			        	}
 			}
 			$timeout($scope.rendermap);
 			$scope.addMarker = function(){
 				$('*').css('cursor', "crosshair");
-				//var zeMap = L.mapbox.map();
-				zeMap.on('click', function(e) {
-					
-			    var latitude = e.latlng.lat;
-			    var longitude = e.latlng.lng;
-			    console.log(latitude + " - " + longitude);
-			    L.marker([latitude, longitude]).addTo(zeMap);
-				   console.log( $scope.singlemapdata.geoJSON); 
+					//var zeMap = L.mapbox.map();
+					zeMap.on('click', function(e) {
+						
+				    var latitude = e.latlng.lat;
+				    var longitude = e.latlng.lng;
+				    console.log(latitude + " - " + longitude);
+				    L.marker([latitude, longitude],{"marker-color": "#ffc423"}).addTo(zeMap);
+				    var newMarker = {};
+				    newMarker = {
+						  "type": "Feature",
+						  "geometry": {
+							    "type": "Point",
+							    "coordinates": [longitude, latitude]
+							  },
+						  "properties": {
+							    "name": "Default"
+
+
+							  }
+						},
+
+					$scope.singlemapdata.geoJSON.push(newMarker);
+				
+					console.log($scope.singlemapdata.geoJSON);
+					localStorage.setItem('singlemapdata'+ $routeParams.mapNo, JSON.stringify($scope.singlemapdata));
+
 			    });
 
 			};
